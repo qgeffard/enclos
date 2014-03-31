83b19bd835f6e30b6c4752d8d0ede6213568329c
CarreauClement <carreauclement@gmail.com>
2014-03-31 13:35:39 +0200

    - bug fix : user can't drop a barrier if there is already a barrier on the bridge

--- src/com/enclos/ui/Enclos.java
+++ src/com/enclos/ui/Enclos.java
@@ -2,8 +2,6 @@ package com.enclos.ui;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,9 +9,7 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.io.ObjectInputStream.GetField;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -26,199 +22,189 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JPanel;
 
-import org.json.simple.JSONArray;
-
-import com.enclos.component.Bridge;
 import com.enclos.controller.State;
 import com.enclos.data.Player;
-import com.enclos.data.SimpleReader;
 import com.enclos.data.SimpleWriter;
 import com.enclos.resources.song.Speaker;
 
 public class Enclos extends JFrame {
 
-	private State state = null;
-	private ScoreFrame scoreFrame = null;
-	private FrameContentPane contentPane = null;
-	private List<Board> boards = new LinkedList<Board>();
-	private List<Player> players = new ArrayList<>();
+    private State state = null;
+    private ScoreFrame scoreFrame = null;
+    private FrameContentPane contentPane = null;
+    private final List<Board> boards = new LinkedList<Board>();
+    private final List<Player> players = new ArrayList<>();
 
-	public Enclos() {
-		this.state = new State(this);
-		this.scoreFrame = new ScoreFrame(this);
-		Toolkit.getDefaultToolkit().setDynamicLayout(false);
-		setTitle("Jeu de l'enclos");
+    public Enclos() {
+        this.state = new State(this);
+        this.scoreFrame = new ScoreFrame(this);
+        Toolkit.getDefaultToolkit().setDynamicLayout(false);
+        setTitle("Jeu de l'enclos");
 
-		// � revoir
-		// setMaximumSize(getScreenMaximumSize());
-		// enabled tab listener
-		setFocusTraversalKeysEnabled(false);
-		setSize(500, 500);
+        // � revoir
+        // setMaximumSize(getScreenMaximumSize());
+        // enabled tab listener
+        setFocusTraversalKeysEnabled(false);
+        setSize(500, 500);
 
-		this.contentPane = new FrameContentPane(this);
+        this.contentPane = new FrameContentPane(this);
 
-		setContentPane(this.contentPane);
-		this.boards.add(new Board(3L, 3));
-		contentPane.addToGamePanel(boards.get(0));
+        setContentPane(this.contentPane);
+        this.boards.add(new Board(3L, 3));
+        contentPane.addToGamePanel(boards.get(0));
 
-		// SimpleReader reader = new SimpleReader("2014-03-28_00-14-17");
-		// Map<String, Object> params = reader.read();
-		//
-		// // LOAD
-		// long loadBoardSize = (long) params.get("Boardsize");
-		// List<JSONArray> barriers = (List<JSONArray>) params.get("Barriers");
-		// List<JSONArray> sheepPositions = (List<JSONArray>)
-		// params.get("Sheepspositions");
-		// Board loadBoard = new Board(loadBoardSize, sheepPositions.size());
-		// this.boards.add(loadBoard);
-		// contentPane.add(loadBoard);
-		// loadBoard.setData(barriers, sheepPositions);
-		// LOAD
+        // SimpleReader reader = new SimpleReader("2014-03-28_00-14-17");
+        // Map<String, Object> params = reader.read();
+        //
+        // // LOAD
+        // long loadBoardSize = (long) params.get("Boardsize");
+        // List<JSONArray> barriers = (List<JSONArray>) params.get("Barriers");
+        // List<JSONArray> sheepPositions = (List<JSONArray>)
+        // params.get("Sheepspositions");
+        // Board loadBoard = new Board(loadBoardSize, sheepPositions.size());
+        // this.boards.add(loadBoard);
+        // contentPane.add(loadBoard);
+        // loadBoard.setData(barriers, sheepPositions);
+        // LOAD
 
-		// contentPane.add(loadBoard);
+        // contentPane.add(loadBoard);
 
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		generateMenu();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        generateMenu();
 
-		addComponentListener(new ComponentAdapter() {
-			@Override
-			public void componentResized(ComponentEvent e) {
-				super.componentResized(e);
-				state.setSize(getSize(), true);
-				scoreFrame.setSize(getSize());
-			}
-		});
+        addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
+                state.setSize(getSize(), true);
+                scoreFrame.setSize(getSize());
+            }
+        });
 
-		addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyPressed(KeyEvent e) {
-				super.keyPressed(e);
-				if (e.getKeyCode() == KeyEvent.VK_TAB) {
-					if (!scoreFrame.isVisible()) {
-						scoreFrame.setLocation(getLocation());
-						scoreFrame.setVisible(true);
-						requestFocus();
-					}
-				}
-				if (e.getKeyCode() == KeyEvent.VK_P) {
-					System.out.println("pressed");
-					Enclos.this.contentPane.switchPanel();
-				}
-			}
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode() == KeyEvent.VK_TAB) {
+                    if (!scoreFrame.isVisible()) {
+                        scoreFrame.setLocation(getLocation());
+                        scoreFrame.setVisible(true);
+                        requestFocus();
+                    }
+                }
+                if (e.getKeyCode() == KeyEvent.VK_P) {
+                    System.out.println("pressed");
+                    Enclos.this.contentPane.switchPanel();
+                }
+            }
 
-			@Override
-			public void keyReleased(KeyEvent e) {
-				super.keyPressed(e);
-				if (e.getKeyCode() == KeyEvent.VK_TAB) {
-					scoreFrame.setVisible(false);
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode() == KeyEvent.VK_TAB) {
+                    scoreFrame.setVisible(false);
 
-					System.out.println("released");
-				}
-			}
-		});
+                    System.out.println("released");
+                }
+            }
+        });
 
-		setVisible(true);
-	}
+        setVisible(true);
+    }
 
-	private void generateMenu() {
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBackground(Color.black);
+    private void generateMenu() {
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setBackground(Color.black);
 
-		JMenu menu = new JMenu("Game");
-		menu.setForeground(Color.white);
-		final JMenuItem newGameItem = new JMenuItem("New Game");
-		final JMenuItem scoreItem = new JMenuItem("Scores");
-		final JMenuItem playerItem = new JMenuItem("Players");
-		final JMenuItem saveItem = new JMenuItem("Save");
-		final JMenuItem soundsItem = new JCheckBoxMenuItem("Play sounds",true);
-		menu.add(newGameItem);
-		menu.add(scoreItem);
-		menu.add(playerItem);
-		menu.add(saveItem);
-		menu.add(soundsItem);
-		menuBar.add(menu);
-		setJMenuBar(menuBar);
+        JMenu menu = new JMenu("Game");
+        menu.setForeground(Color.white);
+        final JMenuItem newGameItem = new JMenuItem("New Game");
+        final JMenuItem scoreItem = new JMenuItem("Scores");
+        final JMenuItem playerItem = new JMenuItem("Players");
+        final JMenuItem saveItem = new JMenuItem("Save");
+        final JMenuItem soundsItem = new JCheckBoxMenuItem("Play sounds", true);
+        menu.add(newGameItem);
+        menu.add(scoreItem);
+        menu.add(playerItem);
+        menu.add(saveItem);
+        menu.add(soundsItem);
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
 
-		newGameItem.addActionListener(new ActionListener() {
+        newGameItem.addActionListener(new ActionListener() {
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// on recup les param�tres du nouveau game
-				Map<String, String> settings = NewGameForm.display();
-				// si on a pas fait cancel
-				if (settings != null) {
-					String boardSize = settings.get("boardSize");
-					// on cr�e le board qui va bien
-					Board newGame = new Board(Integer.valueOf(boardSize), 3);
-					boolean close = settings.get("close").equals("close") ? true
-							: false;
-					// si on a decid� de close les autre jeux
-					if (close) {
-						// on jarte les autres jeux
-						Enclos.this.contentPane.removeAll();
-						Enclos.this.contentPane.add(newGame);
-					} else {
-						// sinon on ajoute le jeu apr�s les autres
-						Enclos.this.contentPane.add(newGame);
-					}
-				}
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // on recup les param�tres du nouveau game
+                Map<String, String> settings = NewGameForm.display();
+                // si on a pas fait cancel
+                if (settings != null) {
+                    String boardSize = settings.get("boardSize");
+                    // on cr�e le board qui va bien
+                    Board newGame = new Board(Integer.valueOf(boardSize), 3);
+                    boolean close = settings.get("close").equals("close") ? true : false;
+                    // si on a decid� de close les autre jeux
+                    if (close) {
+                        // on jarte les autres jeux
+                        Enclos.this.contentPane.resetGamePanel();
+                        Enclos.this.contentPane.addToGamePanel(newGame);
+                    } else {
+                        // sinon on ajoute le jeu apr�s les autres
+                        Enclos.this.contentPane.addToGamePanel(newGame);
+                    }
+                }
 
-			}
-			
-			
-		});
+            }
 
-		playerItem.addActionListener(new ActionListener() {
+        });
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Speaker.playClickEvent();
-				PlayersFrame playersFrame = new PlayersFrame();
-			}
-		});
+        playerItem.addActionListener(new ActionListener() {
 
-		saveItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Speaker.playClickEvent();
+                PlayersFrame playersFrame = new PlayersFrame();
+            }
+        });
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				DateFormat dateFormat = new SimpleDateFormat(
-						"yyyy-MM-dd_HH-mm-ss");
-				Date date = new Date();
-				SimpleWriter writer = new SimpleWriter(boards.get(0),
-						dateFormat.format(date));
-				System.out.println(dateFormat.format(date));
+        saveItem.addActionListener(new ActionListener() {
 
-				if (players.size() > 0) {
-					SimpleWriter playerWriter = new SimpleWriter(players,
-							"players_test");
-				}
-			}
-		});
-		
-		soundsItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+                Date date = new Date();
+                SimpleWriter writer = new SimpleWriter(boards.get(0), dateFormat.format(date));
+                System.out.println(dateFormat.format(date));
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Speaker.isMute(!soundsItem.isSelected());
-			}
-		});
+                if (players.size() > 0) {
+                    SimpleWriter playerWriter = new SimpleWriter(players, "players_test");
+                }
+            }
+        });
 
-	}
+        soundsItem.addActionListener(new ActionListener() {
 
-	private Dimension getScreenMaximumSize() {
-		Dimension maxDimension = null;
-		Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
-		if (screenDimension.getHeight() > screenDimension.getWidth())
-			return new Dimension(screenDimension.width, screenDimension.width);
-		else
-			return new Dimension(screenDimension.height, screenDimension.height);
-	}
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Speaker.isMute(!soundsItem.isSelected());
+            }
+        });
 
-	public List<Player> GetPlayers() {
-		return this.players;
-	}
+    }
+
+    private Dimension getScreenMaximumSize() {
+        Dimension maxDimension = null;
+        Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
+        if (screenDimension.getHeight() > screenDimension.getWidth())
+            return new Dimension(screenDimension.width, screenDimension.width);
+        else
+            return new Dimension(screenDimension.height, screenDimension.height);
+    }
+
+    public List<Player> GetPlayers() {
+        return this.players;
+    }
 
 }
--- src/com/enclos/ui/FrameContentPane.java
+++ src/com/enclos/ui/FrameContentPane.java
@@ -4,12 +4,10 @@ import java.awt.CardLayout;
 import java.awt.FileDialog;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFileChooser;
@@ -22,96 +20,92 @@ import javax.swing.JTextField;
 import com.enclos.data.Player;
 
 public class FrameContentPane extends JPanel {
-	private CardLayout cardLayout;
-	private JPanel gamePanel;
-	private JPanel playersPanel;
-	private Enclos parent;
-	
-	public FrameContentPane(Enclos parent) {
-		this.parent = parent;
-		
-		cardLayout = new CardLayout();
-		this.setLayout(cardLayout);
+    private CardLayout cardLayout;
+    private JPanel gamePanel;
+    private JPanel playersPanel;
+    private Enclos parent;
 
-		gamePanel = new JPanel();
-		gamePanel.setLayout(new FlowLayout());
+    public FrameContentPane(Enclos parent) {
+        this.parent = parent;
 
-		playersPanel = new JPanel();
-		playersPanel.setLayout(new FlowLayout());
+        cardLayout = new CardLayout();
+        this.setLayout(cardLayout);
 
-		JButton addPlayerButton = new JButton("Add player");
-		
-		//only workaround I found to keep the 'P' shortcut after clicking the button
-		addPlayerButton.setFocusable(false);
-		
-		addPlayerButton.addActionListener(new ActionListener() {
+        gamePanel = new JPanel();
+        gamePanel.setLayout(new FlowLayout());
 
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				Player newPlayer = createNewPlayer();
-				PlayerProfilePanel playerProfile = new PlayerProfilePanel(newPlayer);
-				FrameContentPane.this.parent.GetPlayers().add(newPlayer);
-				FrameContentPane.this.playersPanel.add(playerProfile);
-				
-				revalidate();
-			}
+        playersPanel = new JPanel();
+        playersPanel.setLayout(new FlowLayout());
 
-			private Player createNewPlayer() {
-				Player newPlayer = null;
-				final JTextField firstName = new JTextField();
-				final JTextField lastName = new JTextField();
-				final JTextField age = new JTextField();
-				final JButton profilPictureButton = new JButton("Choose a profile picture");
+        JButton addPlayerButton = new JButton("Add player");
 
-				final JFileChooser fileChooser = new JFileChooser();
-				
-				final FileDialog fileDialog = new FileDialog(new JFrame(),"Choose picture", FileDialog.LOAD);
-				profilPictureButton.addActionListener(new ActionListener() {
+        // only workaround I found to keep the 'P' shortcut after clicking the button
+        addPlayerButton.setFocusable(false);
 
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						//fileDialog.setVisible(true);
-						fileChooser.showDialog(FrameContentPane.this, "ok");
-					}
-				});
-				final JComponent[] inputs = new JComponent[] {
-						new JLabel("First Name"),
-						firstName,
-						new JLabel("Last Name"),
-						lastName,
-						new JLabel("Age"),
-						age,
-						profilPictureButton
-					};
-				JOptionPane.showMessageDialog(null, inputs, "Add a new player", JOptionPane.PLAIN_MESSAGE);
-			try{
-				File file = fileChooser.getSelectedFile();
-				System.out.println(file.getAbsolutePath());
-				newPlayer = new Player(firstName.getText(), lastName.getText(), Integer.parseInt(age.getText()), file.getAbsolutePath());
+        addPlayerButton.addActionListener(new ActionListener() {
 
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                Player newPlayer = createNewPlayer();
+                PlayerProfilePanel playerProfile = new PlayerProfilePanel(newPlayer);
+                FrameContentPane.this.parent.GetPlayers().add(newPlayer);
+                FrameContentPane.this.playersPanel.add(playerProfile);
 
-			}catch(Exception e){
-				newPlayer = new Player(firstName.getText(), lastName.getText(), 0);
-			}
-				return newPlayer;
-			}
-		});
+                revalidate();
+            }
 
-		playersPanel.add(addPlayerButton);
-		this.add(gamePanel);
-		this.add(playersPanel);
-	}
+            private Player createNewPlayer() {
+                Player newPlayer = null;
+                final JTextField firstName = new JTextField();
+                final JTextField lastName = new JTextField();
+                final JTextField age = new JTextField();
+                final JButton profilPictureButton = new JButton("Choose a profile picture");
 
-	@Override
-	public void paintComponent(Graphics g) {
-		// g.drawImage(this.background, 0, 0, null);
-	}
+                final JFileChooser fileChooser = new JFileChooser();
 
-	public void addToGamePanel(Board board) {
-		gamePanel.add(board);
-	}
+                final FileDialog fileDialog = new FileDialog(new JFrame(), "Choose picture", FileDialog.LOAD);
+                profilPictureButton.addActionListener(new ActionListener() {
 
-	public void switchPanel() {
-		cardLayout.next(this);
-	}
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        // fileDialog.setVisible(true);
+                        fileChooser.showDialog(FrameContentPane.this, "ok");
+                    }
+                });
+                final JComponent[] inputs = new JComponent[] { new JLabel("First Name"), firstName, new JLabel("Last Name"), lastName,
+                        new JLabel("Age"), age, profilPictureButton };
+                JOptionPane.showMessageDialog(null, inputs, "Add a new player", JOptionPane.PLAIN_MESSAGE);
+                try {
+                    File file = fileChooser.getSelectedFile();
+                    System.out.println(file.getAbsolutePath());
+                    newPlayer = new Player(firstName.getText(), lastName.getText(), Integer.parseInt(age.getText()), file.getAbsolutePath());
+
+                } catch (Exception e) {
+                    newPlayer = new Player(firstName.getText(), lastName.getText(), 0);
+                }
+                return newPlayer;
+            }
+        });
+
+        playersPanel.add(addPlayerButton);
+        this.add(gamePanel);
+        this.add(playersPanel);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        // g.drawImage(this.background, 0, 0, null);
+    }
+
+    public void addToGamePanel(Board board) {
+        gamePanel.add(board);
+    }
+
+    public void resetGamePanel() {
+        gamePanel.removeAll();
+    }
+
+    public void switchPanel() {
+        cardLayout.next(this);
+    }
 }
