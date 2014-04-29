0680501ad9997b07157e6edf71abff7f52c87b22
CarreauClement <carreauclement@gmail.com>
2014-04-29 13:43:30 +0200

    [CHANGES] - when player wins -> saves players (with score updated) -> refresh scorePanel and playersPanel to display the new scores

--- src/com/enclos/ui/Enclos.java
+++ src/com/enclos/ui/Enclos.java
@@ -6,11 +6,9 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.io.ObjectInputStream.GetField;
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 
 import com.enclos.component.EnclosMenu;
@@ -19,122 +17,125 @@ import com.enclos.data.SimpleReader;
 
 public class Enclos extends JFrame {
 
-	// private final State state = null;
-	private ScoreFrame scoreFrame = null;
-	private FrameContentPane contentPane = null;
-	private final List<Board> boards = new LinkedList<Board>();
-	private final List<Player> players;
+    // private final State state = null;
+    private ScoreFrame scoreFrame = null;
+    private FrameContentPane contentPane = null;
+    private final List<Board> boards = new LinkedList<Board>();
+    private final List<Player> players;
 
-	public Enclos() {
-		// si jamais on veut utiliser le principe de la fen�tre carr�e
-		// this.state = new State(this);
-		this.scoreFrame = new ScoreFrame(this);
-		Toolkit.getDefaultToolkit().setDynamicLayout(false);
-		setTitle("Jeu de l'enclos");
+    public Enclos() {
+        // si jamais on veut utiliser le principe de la fen�tre carr�e
+        // this.state = new State(this);
+        this.scoreFrame = new ScoreFrame(this);
+        Toolkit.getDefaultToolkit().setDynamicLayout(false);
+        setTitle("Jeu de l'enclos");
 
-		this.players = SimpleReader.readPlayer("players");
+        this.players = SimpleReader.readPlayer("players");
 
-		// enabled tab listener
-		setFocusTraversalKeysEnabled(false);
-		setSize(500, 500);
+        // enabled tab listener
+        setFocusTraversalKeysEnabled(false);
+        setSize(500, 500);
 
-		this.contentPane = new FrameContentPane(this);
+        this.contentPane = new FrameContentPane(this);
 
-		setContentPane(this.contentPane);
+        setContentPane(this.contentPane);
 
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		EnclosMenu menu = new EnclosMenu(this);
-		this.setJMenuBar(menu);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        EnclosMenu menu = new EnclosMenu(this);
+        this.setJMenuBar(menu);
 
-		addComponentListener(new ComponentAdapter() {
-			@Override
-			public void componentResized(ComponentEvent e) {
-				super.componentResized(e);
-				// state.setSize(getSize(), true);
-				scoreFrame.setSize(getSize());
-			}
-		});
+        addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
+                // state.setSize(getSize(), true);
+                scoreFrame.setSize(getSize());
+            }
+        });
 
-		addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyPressed(KeyEvent e) {
-				super.keyPressed(e);
-				if (e.getKeyCode() == KeyEvent.VK_TAB) {
-					if (!scoreFrame.isVisible()) {
-						scoreFrame.setLocation(getLocation());
-						scoreFrame.setVisible(true);
-						requestFocus();
-					}
-				}
-				if (e.getKeyCode() == KeyEvent.VK_P) {
-					Enclos.this.contentPane.goToPlayersPanel();
-					Enclos.this.contentPane.setPlayersPanelSelectable(false);
-				}
-				if (e.getKeyCode() == KeyEvent.VK_G) {
-					Enclos.this.contentPane.goToGamePanel();
-					Enclos.this.contentPane.setPlayersPanelSelectable(false);
-				}
-				if (e.getKeyCode() == KeyEvent.VK_S) {
-					Enclos.this.contentPane.goToScorePanel();
-					Enclos.this.contentPane.setPlayersPanelSelectable(false);
-				}
-				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-					Enclos.this.contentPane.displayNextGame();
-				}
-				if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-					Enclos.this.contentPane.displayPreviousGame();
-				}
-			}
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode() == KeyEvent.VK_TAB) {
+                    if (!scoreFrame.isVisible()) {
+                        scoreFrame.setLocation(getLocation());
+                        scoreFrame.setVisible(true);
+                        requestFocus();
+                    }
+                }
+                if (e.getKeyCode() == KeyEvent.VK_P) {
+                    Enclos.this.contentPane.goToPlayersPanel();
+                    Enclos.this.contentPane.setPlayersPanelSelectable(false);
+                }
+                if (e.getKeyCode() == KeyEvent.VK_G) {
+                    Enclos.this.contentPane.goToGamePanel();
+                    Enclos.this.contentPane.setPlayersPanelSelectable(false);
+                }
+                if (e.getKeyCode() == KeyEvent.VK_S) {
+                    Enclos.this.contentPane.goToScorePanel();
+                    Enclos.this.contentPane.setPlayersPanelSelectable(false);
+                }
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    Enclos.this.contentPane.displayNextGame();
+                }
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    Enclos.this.contentPane.displayPreviousGame();
+                }
+            }
 
-			@Override
-			public void keyReleased(KeyEvent e) {
-				super.keyPressed(e);
-				if (e.getKeyCode() == KeyEvent.VK_TAB) {
-					scoreFrame.setVisible(false);
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode() == KeyEvent.VK_TAB) {
+                    scoreFrame.setVisible(false);
 
-					System.out.println("released");
-				}
-			}
-		});
+                    System.out.println("released");
+                }
+            }
+        });
 
-		setVisible(true);
-	}
+        setVisible(true);
+    }
 
-	private Dimension getScreenMaximumSize() {
-		Dimension maxDimension = null;
-		Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
-		if (screenDimension.getHeight() > screenDimension.getWidth())
-			return new Dimension(screenDimension.width, screenDimension.width);
-		else
-			return new Dimension(screenDimension.height, screenDimension.height);
-	}
+    private Dimension getScreenMaximumSize() {
+        Dimension maxDimension = null;
+        Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
+        if (screenDimension.getHeight() > screenDimension.getWidth())
+            return new Dimension(screenDimension.width, screenDimension.width);
+        else
+            return new Dimension(screenDimension.height, screenDimension.height);
+    }
 
-	public List<Player> getPlayers() {
-		return this.players;
-	}
+    public List<Player> getPlayers() {
+        return this.players;
+    }
 
-	public FrameContentPane getFrameContentPane() {
-		return this.contentPane;
-	}
+    public FrameContentPane getFrameContentPane() {
+        return this.contentPane;
+    }
 
-	public List<Board> getBoards() {
-		return this.boards;
-	}
+    public List<Board> getBoards() {
+        return this.boards;
+    }
 
-	public Player getCorrespondingPlayer(String firstName, String lastName) {
-		for(Player player : players){
-			if(player.getLastName().equals(lastName) && player.getFirstName().equals(firstName))
-			{
-				return player;
-			}
-		}
-		return null;
-	}
+    public Player getCorrespondingPlayer(String firstName, String lastName) {
+        for (Player player : players) {
+            if (player.getLastName().equals(lastName) && player.getFirstName().equals(firstName)) {
+                return player;
+            }
+        }
+        return null;
+    }
 
-	public void refreshMenu() {
-		EnclosMenu menu = new EnclosMenu(this);
-		this.setJMenuBar(menu);
-	}
+    public void refreshMenu() {
+        EnclosMenu menu = new EnclosMenu(this);
+        this.setJMenuBar(menu);
+    }
+
+    public void refreshPlayersInfo() {
+        getFrameContentPane().refreshPlayersInfo(players);
+    }
 
 }
